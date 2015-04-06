dabd02931e4cd4fbe19cdfffe87bd65aec8bc7a0
Zach Varberg <Zachary.Varberg@Digi.com>
2015-04-06 15:33:16 -0500

    Implement find and get methods as well as the PartialEq trait

--- src/linked_list.rs
+++ src/linked_list.rs
@@ -46,41 +46,114 @@ impl<T> Node<T>
     fn insert_val(&self, val: T) -> Node<T> {
         Node { data: val, next: Option::Some(Rc::new(self.clone())) }
     }
-}
 
-impl<T> Clone for Node<T> 
-           where T: Clone + PartialEq + Display{
-    fn clone(&self) -> Node<T> {
-        let copy_val = self.data.clone();
+    fn length(&self) -> i32 {
         match self.next {
-            Option::Some(ref n) => {
-                Node { data: copy_val, next:  Option::Some(n.clone())}
+            Option::Some(ref n) => 1 + n.length(),
+            Option::None => 1
+        }
+    }
 
-            },
-            Option::None => {
-                Node { data: copy_val, next: Option::None}
+    fn get(&self, idx: i32) -> Node<T> {
+        if idx == 0 {
+            self.clone()
+        } else {
+            match self.next {
+                Option::Some(ref n) => n.get(idx-1),
+                Option::None => panic!("Index out of bounds.")
+            }
+        }
+    }
+
+    fn find(&self, val: T) -> i32 {
+        // return the position of the value passed in
+        // -1 if it is not found
+        if self.data == val {
+            0
+        } else {
+            match self.next {
+                Option::Some(ref n) => {
+                    let pos = n.find(val);
+                    if pos == -1 {
+                        -1
+                    } else {
+                        1 + pos
+                    }
+                },
+                Option::None => -1
             }
         }
     }
 }
 
+impl<T> PartialEq for Node<T>
+    where T: Clone + PartialEq + Display {
+        fn eq(&self, other: &Self) -> bool {
+            match self.next {
+                Option::Some(ref n) => {
+                    match other.next {
+                        Option::Some(ref m) => {
+                            (self.data == other.data) && (n.eq(m))
+                        },
+                        Option::None => false
+                    }
+                },
+                Option::None => {
+                    match other.next {
+                        Option::Some(_) => false,
+                        Option::None => self.data == other.data
+                    }
+                }
+            }
+        }
 
-fn main() {
+        fn ne(&self, other: &Self) -> bool {
+            !self.eq(other)
+        }
+}
+
+impl<T> Clone for Node<T> 
+    where T: Clone + PartialEq + Display{
+        fn clone(&self) -> Node<T> {
+            let copy_val = self.data.clone();
+            match self.next {
+                Option::Some(ref n) => {
+                    Node { data: copy_val, next:  Option::Some(n.clone())}
+
+                },
+                Option::None => {
+                    Node { data: copy_val, next: Option::None}
+                }
+            }
+        }
+}
+
+fn create_test_list(len: i32) -> Rc<Node<i32>> {
     let mut last = Rc::new (
-        Node { data: 11, next: Option::None }
+        Node { data: len - 1, next: Option::None }
     );
-    for x in 0..10 {
+    for x in 0..(len - 2) {
         let next_node = Rc::new (
-            Node { data: 10 - x, next: Option::Some(last.clone()) }
+            Node { data: len - 2 - x, next: Option::Some(last.clone()) }
         );
         last = next_node;
     }
-    let start = Rc::new (Node { data: 0, next: Option::Some(last.clone()) } );
+    Rc::new (Node { data: 0, next: Option::Some(last.clone()) } )
+}
+
+
+
+fn main() {
+    let start = create_test_list(10);
     let copy = start.clone();
     start.print();
+    println!("Copy length: {}", copy.length());
     let copy = copy.delete_val(5);
     match copy {
-        Option::Some(ref n) => n.print(),
+        Option::Some(ref n) => {
+            println!("Copy length: {}", n.length());
+            n.print();
+        },
         Option::None => println!("Empty List")
     }
     let start = start.insert_val(12);
