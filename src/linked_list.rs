3e6d37b610e5e8a78f926af27853d440b03c72a4
Zach Varberg <Zachary.Varberg@Digi.com>
2015-04-06 13:30:02 -0500

    Change all the methods to be implemented on the Node struct itself.

--- src/linked_list.rs
+++ src/linked_list.rs
@@ -1,61 +1,63 @@
 use std::rc::Rc;
 use std::option::Option;
+use std::clone::Clone;
 
 struct Node {
     data: i32,
     next: Option<Rc<Node>>,
 }
 
-
-fn pretty_print_list(node: &Option<Rc<Node>>) {
-    match *node {
-        Option::Some(ref n) => {
-            print!("{{ {} }} -> ", n.data);
-            pretty_print_list(&n.next);
-        },
-        Option::None => println!("{{}}")
-    };
-
-}
-
-
-fn copy_list(node: &Option<Rc<Node>>) -> Option<Rc<Node>> {
-    match *node {
-        Option::Some(ref n) => {
-            let copy_val = n.data;
-            let new_node = Rc::new (
-                Node { data: copy_val, next: copy_list(&n.next) }
-                );
-            Option::Some(new_node)
-        },
-        Option::None => Option::None
+impl Node {
+    fn print(&self) {
+        print!("{{ {} }} -> ", self.data);
+        match self.next {
+            Option::Some(ref n) => n.print(),
+            Option::None => println!("{{}}")
+        };
     }
-}
 
-
-fn delete_from_list(node: &Option<Rc<Node>>, val: i32) -> Option<Rc<Node>> {
-    match *node {
-        Option::Some(ref n) => {
-            if n.data == val {
-                copy_list(&n.next)
-            } else {
-                let copy_val = n.data;
-                let new_node = Rc::new (
-                    Node { data: copy_val, next: delete_from_list(&n.next, val) }
-                    );
-                Option::Some(new_node)
+    fn delete_val(&self, val: i32) -> Option<Rc<Node>> {
+        if val == self.data {
+            match self.next {
+                Option::Some(ref n) => {
+                    Option::Some(n.clone())
+                },
+                Option::None => Option::None
             }
-        },
-        Option::None => Option::None
+        } else {
+            match self.next {
+                Option::Some(ref n) => {
+                   Option::Some(Rc::new (
+                        Node {data: self.data, next: n.delete_val(val)}
+                        ))
+                }
+                Option::None => {
+                   Option::Some(Rc::new (
+                        Node { data: self.data, next: Option::None }
+                        ))
+                }
+            }
+        }
+    }
+
+    fn insert_val(&self, val: i32) -> Node {
+        Node { data: val, next: Option::Some(Rc::new(self.clone())) }
     }
 }
 
+impl Clone for Node {
+    fn clone(&self) -> Node{
+        let copy_val = self.data;
+        match self.next {
+            Option::Some(ref n) => {
+                Node { data: copy_val, next:  Option::Some(n.clone())}
 
-fn insert_into_list(node: Option<Rc<Node>>, val: i32) -> Option<Rc<Node>> {
-    let new_node = Rc::new (
-        Node { data: val, next: node }
-        );
-    Option::Some(new_node)
+            },
+            Option::None => {
+                Node { data: copy_val, next: Option::None}
+            }
+        }
+    }
 }
 
 
@@ -70,11 +72,14 @@ fn main() {
         last = next_node;
     }
     let start = Rc::new (Node { data: 0, next: Option::Some(last.clone()) } );
-    let start_opt = Option::Some(start);
-    pretty_print_list(&start_opt);
-    let node = delete_from_list(&start_opt, 5);
-    pretty_print_list(&node);
-    let new_node = insert_into_list(node, 18);
-    pretty_print_list(&new_node);
+    let copy = start.clone();
+    start.print();
+    let copy = copy.delete_val(5);
+    match copy {
+        Option::Some(ref n) => n.print(),
+        Option::None => println!("Empty List")
+    }
+    let start = start.insert_val(12);
+    start.print();
 
 }
