6a08cb074c102e44f864ac673d6890102f446d41
Zach Varberg <Zachary.Varberg@Digi.com>
2015-04-07 08:58:37 -0500

    Some general cleanup based on review comments.
    Don't import Clone and Option as they are in the standard prelude.
    
    Don't prefix Some and None with Option:: as it is unneeded
    
    Change documentation and return values to be more rustic

--- src/linked_list.rs
+++ src/linked_list.rs
@@ -1,6 +1,4 @@
 use std::rc::Rc;
-use std::option::Option;
-use std::clone::Clone;
 use std::cmp::PartialEq;
 use std::fmt::Display;
 
@@ -15,29 +13,29 @@ impl<T> Node<T>
     fn print(&self) {
         print!("{{ {} }} -> ", self.data);
         match self.next {
-            Option::Some(ref n) => n.print(),
-            Option::None => println!("{{}}")
+            Some(ref n) => n.print(),
+            None => println!("{{}}")
         };
     }
 
-    fn delete_val(&self, val: T) -> Option<Rc<Node<T>>> {
-        if val == self.data {
+    fn delete_val(&self, val: &T) -> Option<Rc<Node<T>>> {
+        if val == &self.data {
             match self.next {
-                Option::Some(ref n) => {
-                    Option::Some(n.clone())
+                Some(ref n) => {
+                    Some(n.clone())
                 },
-                Option::None => Option::None
+                None => None
             }
         } else {
             match self.next {
-                Option::Some(ref n) => {
-                   Option::Some(Rc::new (
+                Some(ref n) => {
+                   Some(Rc::new (
                         Node {data: self.data.clone(), next: n.delete_val(val)}
                         ))
                 }
-                Option::None => {
-                   Option::Some(Rc::new (
-                        Node { data: self.data.clone(), next: Option::None }
+                None => {
+                   Some(Rc::new (
+                        Node { data: self.data.clone(), next: None }
                         ))
                 }
             }
@@ -45,45 +43,46 @@ impl<T> Node<T>
     }
 
     fn insert_val(&self, val: T) -> Node<T> {
-        Node { data: val, next: Option::Some(Rc::new(self.clone())) }
+        Node { data: val, next: Some(Rc::new(self.clone())) }
     }
 
     fn length(&self) -> i32 {
         match self.next {
-            Option::Some(ref n) => 1 + n.length(),
-            Option::None => 1
+            Some(ref n) => 1 + n.length(),
+            None => 1
         }
     }
 
+    /// Get a node from the list
+    /// Return the node at position idx 
+    /// Returns Option::None if the index is out of bounds
     fn get(&self, idx: i32) -> Option<Node<T>> {
-        // Return the node at position idx 
-        // Option::None if the index is out of bounds
         if idx == 0 {
-            Option::Some((self.clone()))
+            Some(self.clone())
         } else {
             match self.next {
-                Option::Some(ref n) => n.get(idx-1),
-                Option::None => Option::None
+                Some(ref n) => n.get(idx-1),
+                None => None
             }
         }
     }
 
-    fn find(&self, val: T) -> i32 {
-        // return the position of the value passed in
-        // -1 if it is not found
+    /// Find the position of a value
+    /// Return the position of the first element in the list with value val
+    /// Returns None if the value val is not in the list
+    fn find(&self, val: T) -> Option<i32> {
         if self.data == val {
-            0
+            Some(0)
         } else {
             match self.next {
-                Option::Some(ref n) => {
+                Some(ref n) => {
                     let pos = n.find(val);
-                    if pos == -1 {
-                        -1
-                    } else {
-                        1 + pos
+                    match pos {
+                        Some(ref n) => Some(n + 1),
+                        None => None
                     }
                 },
-                Option::None => -1
+                None => None
             }
         }
     }
@@ -93,18 +92,18 @@ impl<T> PartialEq for Node<T>
     where T: Clone + PartialEq + Display {
         fn eq(&self, other: &Self) -> bool {
             match self.next {
-                Option::Some(ref n) => {
+                Some(ref n) => {
                     match other.next {
-                        Option::Some(ref m) => {
+                        Some(ref m) => {
                             (self.data == other.data) && (n.eq(m))
                         },
-                        Option::None => false
+                        None => false
                     }
                 },
-                Option::None => {
+                None => {
                     match other.next {
-                        Option::Some(_) => false,
-                        Option::None => self.data == other.data
+                        Some(_) => false,
+                        None => self.data == other.data
                     }
                 }
             }
@@ -120,12 +119,12 @@ impl<T> Clone for Node<T>
         fn clone(&self) -> Node<T> {
             let copy_val = self.data.clone();
             match self.next {
-                Option::Some(ref n) => {
-                    Node { data: copy_val, next:  Option::Some(n.clone())}
+                Some(ref n) => {
+                    Node { data: copy_val, next:  Some(n.clone())}
 
                 },
-                Option::None => {
-                    Node { data: copy_val, next: Option::None}
+                None => {
+                    Node { data: copy_val, next: None}
                 }
             }
         }
@@ -133,15 +132,15 @@ impl<T> Clone for Node<T>
 
 fn create_test_list(len: i32) -> Rc<Node<i32>> {
     let mut last = Rc::new (
-        Node { data: len - 1, next: Option::None }
+        Node { data: len - 1, next: None }
     );
     for x in 0..(len - 2) {
         let next_node = Rc::new (
-            Node { data: len - 2 - x, next: Option::Some(last.clone()) }
+            Node { data: len - 2 - x, next: Some(last.clone()) }
         );
         last = next_node;
     }
-    Rc::new (Node { data: 0, next: Option::Some(last.clone()) } )
+    Rc::new (Node { data: 0, next: Some(last.clone()) } )
 }
 
 #[test]
@@ -156,11 +155,8 @@ fn test_length() {
 fn test_get() {
     let list = create_test_list(10);
     for x in 0..list.length() {
-        let get_val = list.get(x);
-        match get_val {
-            Option::Some(ref n) => assert!(n.data == x),
-            Option::None => panic!("Index {} should not be out of bounds", x)
-        }
+        // unwrap will panic if the node isn't retrieved properly
+        assert!(list.get(x).unwrap().data == x);
     }
 }
 
@@ -169,8 +165,8 @@ fn test_get_oob() {
     let list = create_test_list(10);
     let get_val = list.get(15);
     match get_val {
-        Option::Some(_) => panic!("Index {} should be out of bounds!", 15),
-        Option::None => ()
+        Some(_) => panic!("Index {} should be out of bounds!", 15),
+        None => ()
     }
 }
 
@@ -178,14 +174,18 @@ fn test_get_oob() {
 fn test_find_exists() {
     let list = create_test_list(10);
     for x in 0..list.length() {
-        assert!(list.find(x) == x);
+        assert!(list.find(x).unwrap() == x);
     }
 }
 
 #[test]
 fn test_find_not_exists() {
     let list = create_test_list(10);
-    assert!(list.find(15) == -1);
+    let idx = list.find(15);
+    match idx {
+        Some(_) => panic!("Should not find 15"),
+        None => ()
+    }
 }
 
 #[test]
@@ -193,7 +193,7 @@ fn test_eq() {
     let list = create_test_list(10);
     let list2 = create_test_list(10);
     assert!(list == list2);
-    let list3 = Rc::new ( Node { data: 15, next: Option::Some(list2) } );
+    let list3 = Rc::new ( Node { data: 15, next: Some(list2) } );
     assert!(list != list3);
 }
 
@@ -212,11 +212,14 @@ fn test_insert_val() {
     let list2 = list.insert_val(insert_val);
     assert!(list.length() + 1 == list2.length());
     let find_idx = list2.find(insert_val);
-    assert!(find_idx != -1);
-    let get_val = list2.get(find_idx);
+   let get_val = match find_idx {
+                     Some(n) => list2.get(n),
+                     None => panic!("Should have found the value")
+
+                 };
     match get_val {
-        Option::Some(ref n) => assert!(n.data == insert_val),
-        Option::None => panic!("Index should not be out of bounds")
+        Some(ref n) => assert!(n.data == insert_val),
+        None => panic!("Index should not be out of bounds")
     }
 
 }
@@ -225,13 +228,17 @@ fn test_insert_val() {
 fn test_delete_found() {
     let del_value = 5;
     let list = create_test_list(10);
-    let del_list = list.delete_val(del_value);
+    let del_list = list.delete_val(&del_value);
     match del_list {
-        Option::Some(ref n) => {
-            assert!(n.find(del_value) == -1);
+        Some(ref n) => {
+            let find_idx = n.find(del_value);
+            match find_idx {
+                Some(_) => panic!("Should not find deleted value"),
+                None => ()
+            }
             assert!(n.length() == list.length() - 1)
         },
-        Option::None => panic!("Should successfully return a value!")
+        None => panic!("Should successfully return a value!")
     }
 }
 
@@ -239,25 +246,29 @@ fn test_delete_found() {
 fn test_delete_not_found() {
     let del_value = 15;
     let list = create_test_list(10);
-    let del_list = list.delete_val(del_value);
+    let del_list = list.delete_val(&del_value);
     match del_list {
-        Option::Some(ref n) => {
-            assert!(n.find(del_value) == -1);
+        Some(ref n) => {
+            let find_idx = n.find(del_value);
+            match find_idx {
+                Some(_) => panic!("Should not find value, it was never in the list"),
+                None => ()
+            }
             assert!(n.length() == list.length())
         },
-        Option::None => panic!("Should successfully return a value!")
+        None => panic!("Should successfully return a value!")
     }
 }
 
 #[test]
 fn test_delete_only_element() {
-    let list = Rc::new ( Node { data: 0, next: Option::None});
-    let del_list = list.delete_val(0);
+    let list = Rc::new ( Node { data: 0, next: None});
+    let del_list = list.delete_val(&0);
     match del_list {
-        Option::Some(_) => {
+        Some(_) => {
             panic!("The only element should have been deleted!")
         },
-        Option::None => ()
+        None => ()
     }
 }
 
@@ -267,13 +278,13 @@ fn main() {
     let copy = start.clone();
     start.print();
     println!("Copy length: {}", copy.length());
-    let copy = copy.delete_val(5);
+    let copy = copy.delete_val(&5);
     match copy {
-        Option::Some(ref n) => {
+        Some(ref n) => {
             println!("Copy length: {}", n.length());
             n.print();
         },
-        Option::None => println!("Empty List")
+        None => println!("Empty List")
     }
     let start = start.insert_val(12);
     start.print();
